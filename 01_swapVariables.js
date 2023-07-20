b20d0c1b789d35bfc1f83e45452a5e70a31e9dde
ashwinik007 <ashwinikurapatti007@gmail.com>
2023-07-20 17:03:16 +0530

    Committed and Pushed
    Committed and Pushed

---
+++ 01_swapVariables.js
@@ -0,0 +1,25 @@
+
+console.log('<=======================================>');
+var Name1 = "Sweety"
+var Name2 = "Cutie"
+
+    console.log( 'Before Swap =', 'Sweety -', Name1, ' Cutie - ', Name2);
+    var temp = Name1;
+   Name1 = Name2;
+    Name2 = temp;
+    console.log(temp, Name1, Name2);
+    console.log('After Swap =', 'Sweety -', Name1, ' Cutie - ', Name2);
+
+    console.log('<=======================================>');
+   
+
+var num1 = 100; 
+var num2 = 200; 
+var num3 = 300; 
+console.log( 'Before Swap' ,'num1 - ', num1, ' num2 - ', num2, ' num3 - ', num3, );
+var temp = num1;
+num1 = num2;
+num2 = num3;
+num3 = temp;
+console.log(temp, num1, num2, num3);
+console.log( 'After Swap' ,'num1 - ', num1, ' num2 - ', num2, ' num3 - ', num3,);
