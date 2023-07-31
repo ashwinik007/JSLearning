61bcf3b23e881dae679027e54b783d2777e5ca02
ashwinik007 <ashwinikurapatti007@gmail.com>
2023-07-31 15:39:59 +0530

    Committed & pushed
    Committed & pushed

---
+++ 06_while_Assignment01.js
@@ -0,0 +1,46 @@
+console.log(`----------------------------------------------------Assignment 2----------------------------------------------------`);
+
+console.log(`..................From 5 to 15 by incrementing 1----------------`);
+var i = 5;
+while (i<=15) {
+    console.log(i);
+    i++
+}
+console.log(`..................From 50 to 40 by decrementing 1----------------`);
+var i = 50;
+while (i>=40) {
+    console.log(i);
+   i-=1;
+}
+console.log(`..................First 15 odd numbers----------------`);
+var i = 1;
+while (i<=30) {
+    console.log(i);
+    i +=2;
+}
+console.log(`..................First 10 even numbers----------------`);
+var i = 0;
+while (i<20) {
+    console.log(i);
+    i +=2;
+}
+console.log(`..................Table of 5----------------`);
+var i = 5;
+while (i<=50) {
+    console.log(i);
+    i +=5;
+}
+
+console.log(`..................Table of 10----------------`);
+var i = 10;
+while (i<=100) {
+    console.log(i);
+    i +=10;
+}
+
+console.log(`..................Table of 10 in reverse order----------------`);
+var i = 100;
+while (i>=10) {
+    console.log(i);
+    i -=10;
+}
